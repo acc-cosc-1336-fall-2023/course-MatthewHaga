3671fa7d83af992690a50b60a9a0912222b4fd91
MatthewHaga <143046333+MatthewHaga@users.noreply.github.com>
2023-09-16 15:47:58 +0000

    HW 2 Elif Your Score

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,19 @@
-#
+def get_options_ratio(option, total):
+    result = option / total
+    return result
+
+def get_faculty_rating(score):
+    ratio = score
+    unacceptable = .45
+    needs_improvement = .66
+    good = .71
+    very_good = .85
+    excellent = .91
+
+    if ratio < unacceptable: result = 'is Unacceptable...'
+    elif ratio < needs_improvement: result = 'Needs Improvement'
+    elif ratio < good: result =  'is Good'
+    elif ratio < very_good: result =  'is Very Good'
+    elif ratio <= excellent: result = 'is Excellent!'
+
+    return result
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,8 @@
-#
+import decisions
+
+options = int(input('points'))
+total = int(input('rating'))
+
+rating = decisions.get_faculty_rating(decisions.get_options_ratio(options,total))
+
+print('Your Score ' + str(round(decisions.get_options_ratio(options,total)*100)) + ', ' + rating)
