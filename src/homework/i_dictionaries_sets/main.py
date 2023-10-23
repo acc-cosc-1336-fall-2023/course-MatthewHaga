c3b3aa57d056b965c90a682a2eee46d7be51a934
MatthewHaga <143046333+MatthewHaga@users.noreply.github.com>
2023-10-23 23:26:52 +0000

    HW 6 Lists

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,10 +1,1 @@
-import dictionary as d
-
-list1 = ['T','T','T','C','C','A','T','T','T','A']
-list2 = ['G','A','T','T','C','A','T','T','T','C']
-list3 = ['T','T','T','C','C','A','T','T','T','T']
-list4 = ['G','T','T','C','C','A','T','T','T','A']
-
-dataset = [list1, list2, list3, list4]
-
-d.main_menu()
+#
