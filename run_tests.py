af3986385a0e65b9bf264e170f345b49d730b1e2
MatthewHaga <143046333+MatthewHaga@users.noreply.github.com>
2023-10-23 23:35:31 +0000

    HW6 Lists

--- run_tests.py
+++ run_tests.py
@@ -1,1 +1,4 @@
-#
+import unittest
+from tests.examples.g_lists_and_tuples import tests_lists_and_tuples
+suite = unittest.TestLoader().loadTestsFromModule(tests_lists_and_tuples)
+unittest.TextTestRunner(verbosity=2).run(suite)
